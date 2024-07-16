93c9db4d3fb65472923f44077abb617a2fe35489
Laszlo Lukacs <lukacslacko@gmail.com>
2024-07-16 10:56:52 +0200

    Connect

--- conn.py
+++ conn.py
@@ -1,7 +1,38 @@
 import subprocess
 
+from menu import Menu, set_status
+
 def get_devices():
     out = subprocess.check_output(["kdeconnect-cli", "-a", "--id-name-only"]).decode("utf-8")
     devices = [line for line in out.split("\n") if line]
     print(devices)
     return [(part.split()[0], " ".join(part.split()[1:])) for part in devices]
+
+DEVICES = []
+DEVICE_IDX = None
+
+def choose_device(idx):
+    global DEVICE_IDX, DEVICES
+    DEVICE_IDX = idx
+    set_status(DEVICES[DEVICE_IDX][1])
+
+class DeviceMenu(Menu):
+    def enter(self):
+        self.items = []
+        global DEVICES, DEVICE_IDX
+        DEVICES = get_devices()
+        if not DEVICES:
+            DEVICE_IDX = None
+            set_status("No devices")
+            return
+        for device in DEVICES:
+            self.add(Menu(device[1]))
+        choose_device(0)
+    
+    def handle(self, key):
+        item, idx = self._handle(key)
+        if item:
+            return item
+        choose_device(idx)
+        return self
+        
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from conn import get_devices
+from conn import DeviceMenu
 from menu import Menu, set_status
 from keys import get_key_pressed
 
 ROOT = Menu("RASPIPANO")
 CONNECT = Menu("Connect", ROOT)
 PANO = Menu("Panorama", ROOT)
-LIST_DEVICES = Menu("Device", CONNECT)
+LIST_DEVICES = DeviceMenu("Device", CONNECT)
 TEST_HELLO = Menu("Say hello", CONNECT)
 TEST_SPACE = Menu("3 spaces", CONNECT)
 
 current_menu = ROOT
 current_menu.show()
-
-set_status("Hello")
 
 while True:
     key = get_key_pressed()
